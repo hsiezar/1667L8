ef3e57b59b2d9878bd2dc9e51dba26b55c07714d
Harley Siezar <hsiez001@bolt.cs.ucr.edu>
2018-05-23 11:25:32 -0700

    finally done

--- triggers.sql
+++ triggers.sql
@@ -1,1 +1,15 @@
+CREATE SEQUENCE part_number_seq START WITH 50000;
 
+CREATE OR REPLACE FUNCTION add_part() RETURNS "trigger"  AS 
+$BODY$
+BEGIN
+NEW.part_number := nextval('part_number_seq');
+RETURN NEW;
+END ;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+CREATE TRIGGER mytrig 
+BEFORE INSERT ON part_nyc 
+FOR EACH ROW
+EXECUTE PROCEDURE add_part();
